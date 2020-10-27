affbf99a78877875584f6f5c2c3539c9e8554b70
RMDircio <reginadircio@gmail.com>
2020-10-26 19:33:35 -0500

    Sound Issues and Paths corrected

--- SpaceInvader/main.py
+++ SpaceInvader/main.py
@@ -3,6 +3,8 @@ import os
 import random
 import math
 from pygame import mixer
+import os
+os.environ['SDL_AUDIODRIVER'] = 'dsp'
 
 
 # Initialize pygame
@@ -15,28 +17,28 @@ os.environ["SDL_VIDEODRIVER"] = "dummy"
 screen = pygame.display.set_mode((800, 600)) # (width, height)
 
 # background music
-mixer.music.load('romariogrande__alien-dream.wav')
-mixer.music.play(-1) # -1 means play on loop
+# mixer.music.load('romariogrande__alien-dream.wav')
+# mixer.music.play(-1) # -1 means play on loop
 
 # background image
 # background = pygame.image.load('space-background-with-ficti.png')
 # background = pygame.image.load('3d-space-background-with-fictional-planets-night-sky.png')
 # background = pygame.image.load('8725.eps')
-background = pygame.image.load('8725.png')
-background = pygame.image.load('hubble_constellations_galaxy_133115_800x600.png')
+# background = pygame.image.load('8725.png')
+background = pygame.image.load('SpaceInvader/hubble_constellations_galaxy_133115_800x600.png')
 
 # title
 pygame.display.set_caption('Space Invaders')
 
 # set icon
 # Icon is from Icons made by <a href="https://www.flaticon.com/authors/freepik" title="Freepik">Freepik</a> from <a href="https://www.flaticon.com/" title="Flaticon"> www.flaticon.com</a>
-icon = pygame.image.load('space-invaders-icon32.png')
+icon = pygame.image.load('SpaceInvader/space-invaders-icon32.png')
 pygame.display.set_icon(icon)
 
 
 # Player's Ship Icon
 # player spaceship from Icons made by <a href="https://www.flaticon.com/authors/smashicons" title="Smashicons">Smashicons</a> from <a href="https://www.flaticon.com/" title="Flaticon"> www.flaticon.com</a>
-player_img = pygame.image.load('player-space-ship64.png')
+player_img = pygame.image.load('SpaceInvader/player-space-ship64.png')
 # location - middle and lower half of screen
 playerX = 370 # left=0 right=800
 playerY = 500 # top=0 bottom=600
@@ -74,7 +76,7 @@ num_of_aliens = 6
 
 for i in range(num_of_aliens):
     # icon
-    alien_img.append(pygame.image.load('alien.png'))
+    alien_img.append(pygame.image.load('SpaceInvader/alien.png'))
     # location - make this random
     alienX.append(random.randint(0, 735)) # left=0 right=800
     alienY.append(random.randint(50, 150)) # top=0 bottom=600
@@ -88,7 +90,7 @@ def alien(x,y, i):
 
 
 # laser Icon
-laserImg = pygame.image.load('laser.png')
+laserImg = pygame.image.load('SpaceInvader/laser.png')
 laserX = 0
 laserY = 500 # set to equal player
 laserX_change = 0 # laser do not move on the x axis
@@ -142,8 +144,8 @@ while running:
             if event.key == pygame.K_SPACE: # if key pressed is SPACE BAR
                 # keep laser from always being in fire position
                 if laser_state is 'ready': # is laser on screen already? if not move on
-                    laser_sound = mixer.Sound('sunnyflower__laser-gun.wav') # set laser sound
-                    laser_sound.play()
+                    # laser_sound = mixer.Sound('sunnyflower__laser-gun.wav') # set laser sound
+                    # laser_sound.play()
                     laserX = playerX # reset laser X value to equal player
                     fire_laser(laserX, laserY) # fire laser - keep laser in straight line
 
@@ -193,8 +195,8 @@ while running:
         # collision checks
         collision = is_collision(alienX[i], alienY[i], laserX, laserY)
         if collision: # if laser hits
-            boom_sound = mixer.Sound('anomaex__sci-fi-explosion.wav') # set explosion sound
-            boom_sound.play()
+            # boom_sound = mixer.Sound('anomaex__sci-fi-explosion.wav') # set explosion sound
+            # boom_sound.play()
             laserY = 500 # reset laser
             laser_state = 'ready'
             score_value += 1 # add to score for each hit
